d3257c9aea794922453c3c269acde7059e89d192
VinhCongLe <kslecongvinh@gmaiL.com>
2025-02-13 00:27:39 +0700

    xonggiaodiendangnhap

--- QLQuanCafe/Flogin.cs
+++ QLQuanCafe/Flogin.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QLQuanCafe
 {
-    public partial class Form1 : Form
+    public partial class Flogin : Form
     {
-        public Form1()
+        public Flogin()
         {
             InitializeComponent();
         }
